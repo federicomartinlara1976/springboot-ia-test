8fac2c49162dcf864bbb3c762c78ac8e8f4bec77
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2026-02-15 12:13:19 +0100

    Fix sonar smell code and hotspot

--- src/main/java/net/bounceme/chronos/inteligenciaartificial/controller/HomeController.java
+++ src/main/java/net/bounceme/chronos/inteligenciaartificial/controller/HomeController.java
@@ -1,12 +1,12 @@
 package net.bounceme.chronos.inteligenciaartificial.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     
-	@RequestMapping("/")
+	@GetMapping(path="/")
     public String home() {
         return "forward:/index.xhtml";
     }
