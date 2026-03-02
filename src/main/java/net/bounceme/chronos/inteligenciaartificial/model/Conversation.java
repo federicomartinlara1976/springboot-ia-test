9850cd9a531b1ee3cf44b1987363b1eb9133e692
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2026-03-02 08:47:10 +0100

    Mongodb annotations

--- src/main/java/net/bounceme/chronos/inteligenciaartificial/model/Conversation.java
+++ src/main/java/net/bounceme/chronos/inteligenciaartificial/model/Conversation.java
@@ -1,5 +1,9 @@
 package net.bounceme.chronos.inteligenciaartificial.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -7,12 +11,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Document(collection = "conversations")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 public class Conversation {
 
+	@Id
+    @Field("_id")
 	@Getter
 	@Setter
 	private String conversationId;
