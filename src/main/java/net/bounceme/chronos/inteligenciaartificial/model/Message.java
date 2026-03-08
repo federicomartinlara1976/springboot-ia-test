045886f40a31589e59dc35d71de3eab602596df2
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2026-03-08 10:28:38 +0100

    Add messages entity to conversation

--- src/main/java/net/bounceme/chronos/inteligenciaartificial/model/Message.java
+++ src/main/java/net/bounceme/chronos/inteligenciaartificial/model/Message.java
@@ -2,22 +2,32 @@ package net.bounceme.chronos.inteligenciaartificial.model;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "SPRING_AI_CHAT_MEMORY")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Message {
 
 	@EmbeddedId
 	private MessageId id;
+	
+	@ManyToOne
+    @JoinColumn(name = "conversation_id", referencedColumnName = "conversation_id")
+    private Conversation conversation;
 	
 	private String content;
 	
