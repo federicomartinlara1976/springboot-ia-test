f46285846d63c107aef18b7f7cdbcfbc0678808e
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2026-03-28 10:38:01 +0100

    Descripciones en castellano

--- src/main/java/net/bounceme/chronos/inteligenciaartificial/tools/DateTimeTools.java
+++ src/main/java/net/bounceme/chronos/inteligenciaartificial/tools/DateTimeTools.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DateTimeTools {
 
-	@Tool(description = "Get the current date and time in the user's timezone")
+	@Tool(description = "Obtener la hora actual en la zona horaria del usuario")
     public String getCurrentDateTime() {
 		String currentDateTime = LocalDateTime.now().atZone(LocaleContextHolder.getTimeZone().toZoneId()).toString(); 
         log.info("getCurrentDateTime: {}", currentDateTime);
@@ -21,7 +21,7 @@ public class DateTimeTools {
 		return currentDateTime;
     }
 	
-	@Tool(description = "Set a user alarm for the given time, provided in ISO-8601 format")
+	@Tool(description = "Establece una alarma para la hora dada, proporcionada en formato ISO-8601")
     void setAlarm(String time) {
         LocalDateTime alarmTime = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME);
         log.info("Alarm set for {}", alarmTime);
