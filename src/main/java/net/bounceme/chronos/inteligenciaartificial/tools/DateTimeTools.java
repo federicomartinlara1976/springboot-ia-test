6b98a497682843fb5ad9e3184533d35d3e8d0a55
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2026-03-28 10:33:08 +0100

    Test de acción

--- src/main/java/net/bounceme/chronos/inteligenciaartificial/tools/DateTimeTools.java
+++ src/main/java/net/bounceme/chronos/inteligenciaartificial/tools/DateTimeTools.java
@@ -1,12 +1,15 @@
 package net.bounceme.chronos.inteligenciaartificial.tools;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Component
 @Slf4j
 public class DateTimeTools {
 
@@ -16,5 +19,11 @@ public class DateTimeTools {
         log.info("getCurrentDateTime: {}", currentDateTime);
         
 		return currentDateTime;
+    }
+	
+	@Tool(description = "Set a user alarm for the given time, provided in ISO-8601 format")
+    void setAlarm(String time) {
+        LocalDateTime alarmTime = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME);
+        log.info("Alarm set for {}", alarmTime);
     }
 }
